067138e417649b56759a815fe3b00519d7cb4dc5
Chandolkar001 <89240971+Chandolkar001@users.noreply.github.com>
2021-10-14 19:08:04 +0530

    Task completed
    Task completed

--- Task/textForm/views.py
+++ Task/textForm/views.py
@@ -5,6 +5,7 @@ from django.contrib import auth, messages
 from .models import Profile
 import re
 
+
 def index(request):
 	if request.method == 'POST':
 
@@ -69,13 +70,96 @@ def login(request):
 	return render(request, 'textform/login.html')
 
 def userHome(request):
-	if request.method == 'POST':
-		userinput = request.POST.get('userinput')
-		options  = request.POST.get('')
-	return render(request, 'textform/main.html')
 
+	if request.method == 'POST':
+		
+		userinput = request.POST.get('userinput')
+		options  = request.POST.get('choice')
+		print(options)
+		if options == '1':
+			rest = extract_num(userinput)
+		elif options == '2':
+			rest = extract_date(userinput)
+		elif options == '3':
+			rest = extract_quote(userinput)
+		elif options == '4':
+			resttemp  = IPvalidatar(userinput)
+			if resttemp == 0:
+				rest = 'Invalid'
+			elif resttemp == 1:
+				rest = 'Valid class A'
+			elif resttemp == 2:
+				rest = 'Valid class B'
+			elif resttemp == 3:
+				rest = 'Valid class C'
+		elif options == '5':
+			resttemp = MACvalidatar(userinput)
+			if resttemp == 1:
+				rest = 'Valid'
+			else:
+				rest = 'Invalid'
+		elif options == '6':
+			rest = Cam_to_Sna(userinput)
+		print(rest)
+		mval = options
+		return render(request, 'textform/main.html', {"linknum": rest, "option":mval})
+	
+
+	return render(request, 'textform/main.html')
 
 def logout(request):
 	auth.logout(request)
 	messages.info(request, 'You have logged out successfully')
 	return redirect('login')
+
+
+
+def extract_num(uinput):
+    pattern = re.compile(r'\d{3,}')
+    matches = pattern.findall(uinput)
+    return matches
+
+def extract_date(userinput):
+    pattern = re.compile(r'(19|20\d\d)+[-](0[1-9]|1[0-2])[-](0[1-9]|[1-2][0-9]|3[0-1])')
+    matches = pattern.findall(userinput)
+    return matches
+
+def extract_quote(userinput):
+    initlist = []
+    pattern = re.compile(r"(['])([a-zA-Z0-9_.+-;,]+)(['])")
+    matches = pattern.finditer(userinput)
+    for m in matches:
+        x = m.group(2)
+        initlist.append(x)
+    return initlist
+
+def IPvalidatar(userinput):
+    pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')
+    mess = pattern.finditer(userinput)
+    message = bool(pattern.findall(userinput))
+    if message == False:
+        result = 0
+    else:
+        for m in mess:
+            pick = int(m.group(2))
+            if pick > 0 and pick < 127:
+                result = 1
+            elif pick >= 128 and pick <= 191:
+                result = 2
+            elif pick >= 192 and pick <= 223:
+                result = 3
+    
+    return result 
+
+def MACvalidatar(userinput):
+    pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-])" +"{5}([0-9A-Fa-f]{2})|" +"([0-9a-fA-F]{4}\\." +"[0-9a-fA-F]{4}\\." +"[0-9a-fA-F]{4})$")
+    message = bool(pattern.findall(userinput))
+    if message == True:
+        result = 1
+    else:
+        result = 2
+    return result
+
+def Cam_to_Sna(userinput):
+  userinput = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', userinput)
+  return re.sub('([a-z0-9])([A-Z])', r'\1_\2', userinput).lower()
--- txt.py
+++ txt.py
@@ -1,16 +1,17 @@
 import re
-import socket
+# import socket
 text_t = '''
 12 34
 345 56 5 67 
 192.168.0.1
 366.1.2.2
-2002-01-01
-shreyas 2003 hhii 678 djfnjn 3
-fghrufh 45 jnjnrjkg789 iunidfngi 345 2003-12-27
+345
+'2002-01-01'
+'shreyas' 2003 'hhii' 678 djfnjn 3
+fghrufh '45' jnjnrjkg789 iunidfngi 345 2003-12-27
 '''
 
-ips = '110.234.52.124'
+# ips = '110.234.52.124'
 # main components.
 # r'\d{3,}'
 # r'(19|20)\d\d[-](0[1-9]|1[0-2])[-](0[1-9]|[1-2][0-9]|3[0-1])' also \d{4}[-]\d{2}[-]\d{2}
@@ -39,8 +40,8 @@ ips = '110.234.52.124'
 #     print(m)
 
 
-def snake(match):
-    return match.group(1).lower() + "_" + match.group(2).lower()
+# def snake(match):
+#     return match.group(1).lower() + "_" + match.group(2).lower()
 
 words = """MyClass
 MyClassFactory
@@ -51,7 +52,65 @@ myInstance2
 abc
 patternMatcher""".splitlines()
 
-results = [re.sub(r"(.+?)([A-Z])", snake, w, 0) for w in words]
+# results = [re.sub(r"(.+?)([A-Z])", snake, w, 0) for w in words]
 
-for m in results:
-    print(m)
+# for m in results:
+#     print(m)
+ 
+
+def extract_num(userinput):
+    pattern = re.compile(r'\d{3,}')
+    matches = pattern.findall(userinput)
+    return matches
+
+def extract_date(userinput):
+    pattern = re.compile(r'(19|20[0-9][0-9])[-](0[1-9]|1[0-2])[-](0[1-9]|[1-2][0-9]|3[0-1])')
+    matches = pattern.findall(userinput)
+    return matches
+
+def extract_quote(userinput):
+    initlist = []
+    pattern = re.compile(r"(['])([a-zA-Z0-9_.+-]+)(['])")
+    matches = pattern.finditer(userinput)
+    for m in matches:
+        x = m.group(2)
+        initlist.append(x)
+    return initlist
+
+def IPvalidatar(userinput):
+    pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')
+    mess = pattern.finditer(userinput)
+    message = bool(pattern.findall(userinput))
+    if message == False:
+        result = 0
+    else:
+        for m in mess:
+            pick = int(m.group(2))
+            if pick > 0 and pick < 127:
+                result = 1
+            elif pick >= 128 and pick <= 191:
+                result = 2
+            elif pick >= 192 and pick <= 223:
+                result = 3
+    
+    return result 
+
+def MACvalidatar(userinput):
+    pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-])" +"{5}([0-9A-Fa-f]{2})|" +"([0-9a-fA-F]{4}\\." +"[0-9a-fA-F]{4}\\." +"[0-9a-fA-F]{4})$")
+    message = bool(pattern.findall(userinput))
+    if message == True:
+        result = 1
+    else:
+        result = 2
+    return result
+
+def Cam_to_Sna(userinput):
+    def snake(match):
+        return match.group(1).lower() + "_" + match.group(2).lower()
+    
+    result = [re.sub(r"(.+?)([A-Z])", snake, w, 0) for w in userinput]
+    return result
+
+demt = 'camelCase'
+results = Cam_to_Sna(words)
+print(results)
